1df5f43168a7752e184c7458a0f6d99a18efdab2
Sergei Bondarenko <sergei_bondarenko@bose.com>
2020-06-24 17:57:04 +0300

    Added save csv test.

--- tests/test_download_candles.py
+++ tests/test_download_candles.py
@@ -48,6 +48,13 @@ def test_dataframe():
   return pd.DataFrame(data=d).set_index('date')
 
 
+@pytest.fixture
+def csv_file():
+  return ('date,high,low,open,close,volume,quoteVolume,weightedAverage\n'
+    + '1575158400,0.02009497,0.020021,0.02007299,0.02008,2.83754783,141.51364588,0.0200514\n'
+    + '1575160200,0.02014813,0.02007299,0.02008427,0.02012469,0.15556988,7.73633777,0.02010898\n')
+
+
 def test_request(test_candles):
   '''Test request to Poloniex.'''
   candles = download_sample.request('ETH', 1800, 1575158400, 1575160200)
@@ -55,10 +62,19 @@ def test_request(test_candles):
 
 
 def test_candles_to_df(test_candles, test_dataframe):
-  '''Test conversion from candles to Pandas DataFrame.'''
+  '''Test conversion from candles to a Pandas DataFrame.'''
   #assert download_sample.candles_to_df(test_candles) == test_dataframe
   pd.testing.assert_frame_equal(download_sample.candles_to_df(test_candles), test_dataframe)
 
+
+def test_save_csv(test_dataframe, tmp_path, csv_file):
+  '''Test saving a Pandas DataFrame to a csv file.'''
+  filepath = tmp_path / 'test.csv'
+  download_sample.save_csv(test_dataframe, filepath)
+  with open(filepath, 'r') as f:
+    csv = f.read()
+  assert csv_file == csv
+  
 
 def test_request(test_candles):
   '''Test request to Poloniex.'''
