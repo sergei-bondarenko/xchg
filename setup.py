9955614c4df4c9ab2ebb36805be66310387cfcd5
grez911 <grez911@gmail.com>
2020-08-05 22:15:02 +0300

    Added python versions badge.

--- setup.py
+++ setup.py
@@ -4,22 +4,29 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-     name='xchg',
-     version='0.0.3',
-     description='Simulator of a currency exchange.',
-     long_description=long_description,
-     long_description_content_type='text/markdown',
-     author='Sergei Bondarenko',
-     author_email='sergei@bondarenko.xyz',
-     url='https://github.com/sergei-bondarenko/xchg',
-     license='Unlicense',
-     packages=['xchg'],
-     install_requires=[
-         'pandas',
-         'poloniex',
-         'numpy'
-     ],
-     entry_points={
-         'console_scripts': ['download_sample=xchg.download_sample:_main'],
-     }
+    name='xchg',
+    version='1.0.0',
+    description='Simulator of a currency exchange.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Sergei Bondarenko',
+    author_email='sergei@bondarenko.xyz',
+    url='https://github.com/sergei-bondarenko/xchg',
+    license='Unlicense',
+    packages=['xchg'],
+    install_requires=[
+        'pandas',
+        'poloniex',
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts': ['download_sample=xchg.download_sample:_main'],
+    }
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
