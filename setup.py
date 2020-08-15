1be7ff00ce5ff4c0e19cd0d9a4feaa138a532eca
grez911 <grez911@gmail.com>
2020-08-15 14:47:21 +0300

    Added __version__ attribute.

--- setup.py
+++ setup.py
@@ -1,26 +1,47 @@
+import codecs
+import os.path
 from setuptools import setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
+
+
+PACKAGE_NAME = 'xchg'
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='xchg',
-    version='1.0.3',
+    name=PACKAGE_NAME,
+    version=get_version(f"{PACKAGE_NAME}/__init__.py"),
     description='Simulator of a currency exchange.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Sergei Bondarenko',
     author_email='sergei@bondarenko.xyz',
-    url='https://github.com/sergei-bondarenko/xchg',
+    url=f"https://github.com/sergei-bondarenko/{PACKAGE_NAME}",
     license='Unlicense',
-    packages=['xchg'],
+    packages=[PACKAGE_NAME],
     install_requires=[
         'pandas',
         'poloniex',
         'numpy'
     ],
     entry_points={
-        'console_scripts': ['download_sample=xchg.download_sample:_main'],
+        'console_scripts':
+            [f"download_sample={PACKAGE_NAME}.download_sample:_main"],
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
